5025f0bc2e2d58dfc19f8284cbe3970b9e30aae7
sudeepmi <sudeepm0599@gmail.com>
2023-06-08 12:41:42 +0545

    read and update

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -52,6 +52,51 @@ public class ProductController : Controller
         return View(product);
     }
 
+    public IActionResult Edit(string id){
+        using var connection = new MySqlConnection("Server=localhost;User ID=root;Password=password;Database=dotnet");
+        connection.Open();
+        string q = "SELECT * from products WHERE ID='"+id+"'"; 
+        using var command = new MySqlCommand(q, connection);
+        MySqlDataReader reader = command.ExecuteReader();
+        Products product = new Products();
+        while(reader.Read()){
+            product.ID = reader["ID"].ToString();
+            product.Code = reader["Code"].ToString();
+            product.Name = reader["Name"].ToString();
+            product.Price = Convert.ToDouble(reader["Price"]);
+        }
+        return View(product);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Products p){
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        
+        using var connection = new MySqlConnection("Server=localhost;User ID=root;Password=password;Database=dotnet");
+        connection.Open();
+        string q = "UPDATE products SET Name='"+p.Name+"', Code='"+p.Code+"',ID='"+p.ID+"',Price='"+p.Price+"'"; 
+        Console.WriteLine(q);
+        using var command = new MySqlCommand(q, connection);
+        int reader = command.ExecuteNonQuery();
+        Console.WriteLine(reader);
+        ViewData["product"] = "Updated";
+        return View();
+    }
+
+    public IActionResult Delete(Products p){
+       
+        using var connection = new MySqlConnection("Server=localhost;User ID=root;Password=password;Database=dotnet");
+        connection.Open();
+        string q = "DELETE from products WHERE ID='"+p.ID+"'"; 
+        using var command = new MySqlCommand(q, connection);
+        int reader = command.ExecuteNonQuery();
+        Console.WriteLine(reader);
+        ViewData["product"] = "Delete";
+        return Redirect("/Product/");                         
+    }
 
 
 
@@ -71,6 +116,6 @@ public class ProductController : Controller
         int reader = command.ExecuteNonQuery();
         Console.WriteLine(reader);
         ViewData["product"] = "Done";
-        return View();
+        return Redirect("/Product/");        
     }
 }
